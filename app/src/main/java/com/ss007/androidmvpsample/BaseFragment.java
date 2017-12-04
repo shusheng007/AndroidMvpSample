e42ee3b5442e3a46e4edbaa1b38f871458690caf
shusheng007 <568894336@qq.com>
2017-12-04 14:07:00 +0800

    modify file head info

--- app/src/main/java/com/ss007/androidmvpsample/BaseFragment.java
+++ app/src/main/java/com/ss007/androidmvpsample/BaseFragment.java
@@ -3,8 +3,7 @@ package com.ss007.androidmvpsample;
 import android.support.v4.app.Fragment;
 
 /**
- * Copyright (C) 2017 在线回声（天津）科技发展有限公司
- * 在线回声完全享有此软件的著作权，违者必究
+ * Copyright (C) 2017
  *
  * @author ben
  * @version 1.0
